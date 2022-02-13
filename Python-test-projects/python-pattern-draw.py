f46cca7885064d3f4c7871ebb53a2113ecc02927
sourav <souravak211@gmail.com>
2022-02-14 01:07:00 +0530

    python drawing study day 1

---
+++ Python-test-projects/python-pattern-draw.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.speed(0.5)
+turtle.goto(-10,0)
+turtle.goto(-10,-10)
+turtle.goto(0,-10)
+turtle.goto(0,-20)
+turtle.goto(-10,-20)
+#! /usr/bin/python3
+turtle.color('red')
+style = ('Courier', 90, 'normal')
+# turtle.write('SOURAV', font=style, align='center')
+turtle.hideturtle()
+window:object = turtle.Screen()
+window.exitonclick()
